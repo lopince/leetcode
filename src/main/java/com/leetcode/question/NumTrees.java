55f83908e315134ab1bbbc973e1e16f2d4f57dc0
lopince <lopince@163.com>
2020-08-10 14:52:05 +0800

    fix bug

---
+++ src/main/java/com/leetcode/question/NumTrees.java
@@ -0,0 +1,37 @@
+package com.leetcode.question;
+
+public class NumTrees {
+
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+        System.out.println(solution.numTrees(3));
+    }
+
+    private static class Solution {
+
+        public int numTrees(int n) {
+
+            if (n == 0 || n == 1 || n == 2) {
+                return n;
+            }
+
+            // now n>2
+            int[] dp = new int[n + 1];
+
+            // base case
+            dp[0] = 1;
+            dp[1] = 1;
+            dp[2] = 2;
+
+            for (int i = 3; i <= n; i++) {
+                for (int j = 1; j <= i; j++) {
+
+                    dp[i] += (dp[j - 1] * dp[i - j]);
+                }
+            }
+
+            return dp[n];
+        }
+    }
+}
