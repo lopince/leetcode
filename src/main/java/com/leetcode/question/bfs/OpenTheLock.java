d50002c14b97d3e1bfa37112e4d3cbad832386ef
lopince <lopince@163.com>
2020-08-12 17:23:58 +0800

    update

--- src/main/java/com/leetcode/question/bfs/OpenTheLock.java
+++ src/main/java/com/leetcode/question/bfs/OpenTheLock.java
@@ -16,7 +16,7 @@ public class OpenTheLock {
 //        String target = "0009";
 
         Solution solution = new Solution();
-        System.out.println(solution.openLock(deadends, target));
+        System.out.println(solution.openLock2(deadends, target));
     }
 
     private static class Solution {
@@ -29,7 +29,7 @@ public class OpenTheLock {
                     target);
         }
 
-        public int openLock(List<String> deadends, String target) {
+        private int openLock(List<String> deadends, String target) {
 
             String cur = "0000";
 
@@ -50,7 +50,7 @@ public class OpenTheLock {
                     if (target.equals(cur)) {
                         return step;
                     }
-                    if (deadends.contains(cur)){
+                    if (deadends.contains(cur)) {
                         continue;
                     }
 
@@ -76,7 +76,63 @@ public class OpenTheLock {
             return -1;
         }
 
-        public String plus(String cur, int index) {
+        /**
+         * 双向bfs
+         */
+        public int openLock2(String[] deadends, String target) {
+
+            Set<String> deadendSet = new HashSet<>(Arrays.asList(deadends));
+
+            Set<String> q1 = new HashSet<>();
+            Set<String> q2 = new HashSet<>();
+            Set<String> sentinel = new HashSet<>();
+
+            int step = 0;
+            q1.add("0000");
+            q2.add(target);
+
+            while (!q1.isEmpty() && !q2.isEmpty()) {
+
+                if (q1.size() > q2.size()) {
+                    // 交换 q1 和 q2
+                    swap(q1, q2);
+                }
+
+                Set<String> tempSet = new HashSet<>();
+
+                for (String cur : q1) {
+
+                    if (deadendSet.contains(cur)) {
+                        continue;
+                    }
+                    if (q2.contains(cur)) {
+                        return step;
+                    }
+                    sentinel.add(cur);
+
+                    for (int j = 0; j < 4; j++) {
+
+                        String temp = plus(cur, j);
+                        if (!sentinel.contains(temp)) {
+                            tempSet.add(temp);
+                        }
+
+                        temp = minus(cur, j);
+                        if (!sentinel.contains(temp)) {
+                            tempSet.add(temp);
+                        }
+                    }
+                }
+
+                step++;
+                q1 = q2;
+                q2 = tempSet;
+            }
+
+            return -1;
+        }
+
+        private String plus(String cur, int index) {
 
             int val = Integer.parseInt(cur.substring(index, index + 1));
             if (val == 9) {
@@ -88,7 +144,7 @@ public class OpenTheLock {
             return replace(cur, index, val);
         }
 
-        public String minus(String cur, Integer index) {
+        private String minus(String cur, Integer index) {
 
             int val = Integer.parseInt(cur.substring(index, index + 1));
             if (val == 0) {
@@ -100,12 +156,19 @@ public class OpenTheLock {
             return replace(cur, index, val);
         }
 
-        public String replace(String cur, int index, int val) {
+        private String replace(String cur, int index, int val) {
 
             StringBuilder sb = new StringBuilder(cur);
             sb.replace(index, index + 1, String.valueOf(val));
 
             return sb.toString();
+        }
+
+        private <T> void swap(Set<T> set1, Set<T> set2){
+
+            Set<T> temp = set1;
+            set1 = set2;
+            set2 = temp;
         }
     }
 }
