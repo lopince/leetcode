92f90121422333725be63139172942d382786cd9
lopince <lopince@163.com>
2020-08-12 11:31:25 +0800

    update

---
+++ src/main/java/com/leetcode/question/backtrack/FindTargetSumWays.java
@@ -0,0 +1,41 @@
+package com.leetcode.question.backtrack;
+
+public class FindTargetSumWays {
+
+    public static void main(String[] args) {
+
+        int[] nums = new int[]{18, 47, 49, 45, 31, 13, 45, 44, 33, 45, 44, 43, 21, 30, 23, 46, 43, 10, 3, 30};
+        int s = 23;
+
+        Solution solution = new Solution();
+        System.out.println(solution.findTargetSumWays(nums, s));
+    }
+
+    private static class Solution {
+
+        int counter = 0;
+
+        public int findTargetSumWays(int[] nums, int s) {
+
+            backtrack(nums, 0, 0, s);
+
+            return counter;
+        }
+
+
+        private void backtrack(int[] nums, int index, int sum, int target) {
+
+            if (index == nums.length) {
+                if (sum == target) {
+                    counter++;
+                }
+                return;
+            }
+
+            int cur = nums[index];
+
+            backtrack(nums, index + 1, sum + cur, target);
+            backtrack(nums, index + 1, sum - cur, target);
+        }
+    }
+}
