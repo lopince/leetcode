46881bf2875acb1a0cda40f2fe2eec28f95d3aa1
lopince <lopince@163.com>
2020-08-19 16:35:00 +0800

    fix

--- src/main/java/com/leetcode/solution/RemoveDuplicatedFromSortedList.java
+++ src/main/java/com/leetcode/solution/RemoveDuplicatedFromSortedList.java
@@ -26,24 +26,24 @@ public class RemoveDuplicatedFromSortedList {
 
         public ListNode deleteDuplicates(ListNode head) {
 
+            if(head == null){
+                return null;
+            }
+
             ListNode slow = head;
-            ListNode fast = head;
+            ListNode fast = head.next;
 
             while (fast != null) {
 
                 if (slow.val != fast.val) {
-                    slow.val = fast.val;
-
-                    if (fast.next == null) {
-                        slow.next = null;
-                    } else {
-                        slow = slow.next;
-                    }
+                    slow.next = fast;
+                    slow = slow.next;
                 }
 
                 fast = fast.next;
             }
 
+            slow.next = null;
 
             return head;
         }
