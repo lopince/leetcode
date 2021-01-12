73060bccc407ec32b26aa60cf5898548d38dca6a
lopince <lopince@163.com>
2021-01-12 11:04:20 +0800

    update

--- src/main/java/com/leetcode/solution/Sqrtx.java
+++ src/main/java/com/leetcode/solution/Sqrtx.java
@@ -4,15 +4,35 @@ public class Sqrtx {
 
     public static void main(String[] args) {
 
-        int x = 8;
+        int x = 2147395599;
 
         Solution solution = new Solution();
         System.out.println(solution.mySqrt(x));
     }
 
     private static class Solution {
+
         public int mySqrt(int x) {
 
+            long l = 0;
+            long r = x;
+            long ans = -1;
+
+            while (l <= r) {
+
+                System.out.printf("l: %s, r: %s, ans: %s\n", l, r, ans);
+
+                long mid = (l + r) / 2;
+
+                if (mid * mid <= x) {
+                    ans = mid;
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+
+            return (int) ans;
         }
     }
 }
