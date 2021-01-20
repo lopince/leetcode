ae98e03da9997069d3464c57a98502fdf22f89d8
lopince <lopince@163.com>
2021-01-20 15:15:13 +0800

    update

--- src/main/java/com/leetcode/bean/ListNode.java
+++ src/main/java/com/leetcode/bean/ListNode.java
@@ -8,4 +8,27 @@ public class ListNode {
     public ListNode(int x) {
         val = x;
     }
+
+    public static ListNode gen(Integer... vals) {
+
+        ListNode head = null;
+
+        if (vals.length > 0) {
+
+            head = new ListNode(vals[0]);
+
+            if (vals.length > 1) {
+
+                ListNode cur = head;
+                for (int i = 1; i < vals.length; i++) {
+
+                    ListNode temp = new ListNode(vals[i]);
+                    cur.next = temp;
+                    cur = temp;
+                }
+            }
+        }
+
+        return head;
+    }
 }
---
+++ src/main/java/com/leetcode/shopee/AddTwoNumbers.java
@@ -0,0 +1,62 @@
+package com.leetcode.shopee;
+
+import com.leetcode.bean.ListNode;
+
+public class AddTwoNumbers {
+
+    public static void main(String[] args) {
+
+        ListNode l1 = ListNode.gen(9, 9, 9, 9, 9, 9, 9);
+        ListNode l2 = ListNode.gen(9, 9, 9, 9);
+
+        Solution solution = new Solution();
+        ListNode l = solution.addTwoNumbers(l1, l2);
+        while (l != null) {
+            System.out.print(l.val + " ");
+            l = l.next;
+        }
+    }
+
+    private static class Solution {
+
+        public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+            ListNode head = new ListNode(-1);
+            ListNode cur = head;
+
+            int carry = 0;
+
+            while (l1 != null && l2 != null) {
+
+                int val = (l1.val + l2.val + carry) % 10;
+                ListNode temp = new ListNode(val);
+                cur.next = temp;
+                cur = temp;
+
+                carry = (l1.val + l2.val + carry) / 10;
+
+                l1 = l1.next;
+                l2 = l2.next;
+            }
+
+            ListNode rest = l1 == null ? l2 : l1;
+            while (rest != null) {
+
+                int val = (rest.val + carry) % 10;
+                ListNode temp = new ListNode(val);
+                cur.next = temp;
+                cur = temp;
+
+                carry = (rest.val + carry) / 10;
+
+                rest = rest.next;
+            }
+
+            if (carry > 0) {
+                cur.next = new ListNode(carry);
+            }
+
+            return head.next;
+        }
+    }
+}
