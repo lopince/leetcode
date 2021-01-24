9f2801f7dfa2869c476e7ea5df7246cbacad0827
lopince <lopince@163.com>
2021-01-24 21:47:14 +0800

    update

---
+++ src/main/java/com/leetcode/shopee/BinaryTreeRightSideView.java
@@ -0,0 +1,58 @@
+package com.leetcode.shopee;
+
+import com.leetcode.bean.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class BinaryTreeRightSideView {
+
+    public static void main(String[] args) {
+
+        TreeNode root = TreeNode.gen(1, 2, 3, null, 5, null, 4);
+
+        Solution solution = new Solution();
+        System.out.println(solution.rightSideView(root));
+    }
+
+    private static class Solution {
+
+        public List<Integer> rightSideView(TreeNode root) {
+
+            List<Integer> ans = new ArrayList<>();
+
+            if (root == null) {
+                return ans;
+            }
+
+            Queue<TreeNode> queue = new LinkedList<>();
+
+            TreeNode cur = root;
+            queue.offer(cur);
+            while (!queue.isEmpty()) {
+
+                int c = queue.size();
+                Integer rightest = null;
+                while (c-- > 0) {
+
+                    cur = queue.poll();
+                    if (cur.left != null) {
+                        queue.offer(cur.left);
+                    }
+                    if (cur.right != null) {
+                        queue.offer(cur.right);
+                    }
+                    rightest = cur.val;
+                }
+
+                if (rightest != null) {
+                    ans.add(rightest);
+                }
+            }
+
+            return ans;
+        }
+    }
+}
