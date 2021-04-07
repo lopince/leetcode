f41503c093c6d1fde65615a940a40a21a158b7f0
huangyekun <huangyekun@corp.netease.com>
2021-04-07 16:45:17 +0800

    test

--- leetcode_py/ListNode.py
+++ leetcode_py/ListNode.py
@@ -3,7 +3,7 @@ class ListNode(object):
     def __init__(self, val=-1, next=None):
         self.val = val
         self.next = next
-        
+         
     def __str__(self):
         return str(self.val)
     
