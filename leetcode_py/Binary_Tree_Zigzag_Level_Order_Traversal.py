71fd3836c7a051b40c9739ecdeee9b4d118d6a25
huangyekun <huangyekun@corp.netease.com>
2021-04-12 10:44:59 +0800

    update

--- leetcode_py/Binary_Tree_Zigzag_Level_Order_Traversal.py
+++ leetcode_py/Binary_Tree_Zigzag_Level_Order_Traversal.py
@@ -1,4 +1,6 @@
-from 
+from TreeNode import TreeNode
+from Queue import Queue
+
 
 class Solution(object):
     def zigzagLevelOrder(self, root):
@@ -6,5 +8,37 @@ class Solution(object):
         :type root: TreeNode
         :rtype: List[List[int]]
         """
+        if root == None:
+            return []
         
-root = Tr
+        rets = []
+
+        q = Queue()
+        q.put(root)
+
+        while not q.empty():
+
+            ret = []
+
+            size = q.qsize()
+            while size > 0:
+
+                cur = q.get()
+                ret.append(cur.val)
+
+                if cur.right != None:
+                    q.put(cur.right)
+                if cur.left != None:
+                    q.put(cur.left)
+
+                size -= 1
+            rets.append(ret)
+        
+        return rets
+
+
+root = TreeNode.gen([3, 9, 20, None, None, 15, 7])
+print root
+
+s = Solution()
+rets = 
